502a62c2117925779f2b44351aca782e7d690b4e
callmewhy <whywanghai@gmail.com>
2024-11-23 23:53:17 +0800

    no message

--- apps/example/src/pages/index.tsx
+++ apps/example/src/pages/index.tsx
@@ -1,12 +1,16 @@
 import { Safari } from '@/components/safari'
+import { useSearchParams } from 'react-router-dom'
 
 export default function () {
+  const [search] = useSearchParams()
+  const url = search.get('url') ?? `${window.origin}/dapp`
+
   return (
     <div className="flex-center h-95vh">
       <div className="relative mt-5vh h-[753px] w-[1202px]">
-        <Safari url={`${window.origin}/dapp`} width={1203} height={753} className="absolute" />
+        <Safari url={url} width={1203} height={753} className="absolute" />
         <div className="relative z-100 mb-[2px] size-full pt-[52px] overflow-hidden rounded-b-xl">
-          <iframe src="/dapp" className="h-full w-full border-0" />
+          <iframe src={url} className="h-full w-full border-0" />
         </div>
       </div>
     </div>
